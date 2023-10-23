383116a3f5f9272b23da34530f2102caa06d7ba5
DenisaZeka <denisazeka8@gmail.com>
2023-10-23 15:58:51 +0200

    final final

--- admin-page.php
+++ admin-page.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    />
     <title>Admin page</title>
   </head>
   <body>
